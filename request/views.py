1e5818417276e007372d1632d9419b6aad279d63
yuki <31yuki369@gmail.com>
2024-01-23 10:35:46 +0900

    index、完了

--- request/views.py
+++ request/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import requests
 
-# Create your views here.
+def index(request):
+    return render(request, 'request/index.html')
+
+
+def detail(request):
+    #if 'POST' in request.method.lower():
+
+    res = requests.get("https://weather.tsukumijima.net/api/forecast/city/{'location'}")
+    context = {
+        'res':res,
+    }
+
+    return render(request, 'request/detail.html', context)
